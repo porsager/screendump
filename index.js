de7ca19d8b2d5c25cd5475b9ccdb1e525afa1df4
Rasmus Porsager <rasmus@porsager.com>
2024-06-20 21:22:14 +0200

    More compability with cjs

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-import { createRequire } from 'module'
-
-const require = createRequire(import.meta.url)
 const screendump = require('./build/Release/screendump.node')
 
-export default capture
-
-async function capture({ display = -1, scale = 1, quality = 75 } = {}) {
+module.exports = async function capture({ display = -1, scale = 1, quality = 75 } = {}) {
   return new Promise((resolve, reject) => {
     screendump.capture(display, scale, quality, (error, x) => error ? reject(error) : resolve(x))
   })
